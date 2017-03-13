3eb6a7efbadb86324e5ac3bef5c57c4bd499a2a6
Tommy Lincoln <pajamapants3000@gmail.com>
2017-03-13 04:05:06 -0700

    first ergodox layout - tommy_qwerty - update

--- keyboards/ergodox/keymaps/tommy_qwerty/keymap.c
+++ keyboards/ergodox/keymaps/tommy_qwerty/keymap.c
@@ -1,3 +1,11 @@
+/* tommy_qwerty
+ * keymap for the ergodox ez
+ * Currently, none of the KC_MEDIA_* keys seem to do anything
+ * Also, KC_WWW_FAVORITES and KC_ACL* don't seem to do anything either
+ *
+ * There is probably a lot more I can do with this, but it's a nice start!
+ */
+
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
@@ -19,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| C-F12|           |File-M|------+------+------+------+------+--------|
  * | LS/PO  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RS/PC  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Esc  | Grv  | App  | Left | Right|                                       |  Up  | Down |   [  |   ]  |TO-L0 |
+ *   | Esc  | Grv  | App  | Left | Right|                                       |  Up  | Down |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Del  | TO-2 |       | TO-1 | Enter  |
@@ -46,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_WWW_REFRESH,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
                                   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
              KC_MY_COMPUTER,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSPC,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,KC_NO,
+                                  KC_DOWN,KC_UP,KC_LBRC,KC_RBRC,KC_NO,
              TO(1),     KC_ENT,
              KC_END,
              MO(1),     KC_LGUI, KC_SPC
@@ -88,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,       KC_UP,  KC_KP_7,  KC_KP_8,    KC_KP_9,    KC_KP_MINUS,    KC_F12,
                       KC_DOWN,KC_KP_4,  KC_KP_5,    KC_KP_6,    KC_KP_PLUS,     KC_KP_SLASH,
        KC_WWW_SEARCH, KC_DLR, KC_KP_1,  KC_KP_2,    KC_KP_3,    KC_KP_ASTERISK, KC_NUMLOCK,
-                              KC_KP_0,  KC_KP_DOT,  KC_KP_EQUAL,KC_KP_ENTER,TO(0),
+                              KC_KP_0,  KC_KP_DOT,  KC_EQUAL,   KC_KP_ENTER,    TO(0),
        TO(2), KC_TRNS,
        KC_END,
        KC_TRNS, KC_TRNS, KC_TRNS
@@ -96,15 +104,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Win-L  | ???  | Cut  | Copy | Paste|?Find |TRANS |           |TRANS | Prev | Rew  | Play | FF   | Next | Stop   |
+ * | Win-L  | ???  | Cut  | Copy | Paste| Find |TRANS |           |TRANS | Prev | Rew  | Play | FF   | Next | Stop   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | ?Power | ???  | Undo | WlUp | C-Y  | ???  |TRANS |           |TRANS | C-Lft| LClk | MsUp | RClk | C-Rt | Vol-Up |
+ * | ?Power | ???  | Undo | WlUp | C-Y  | C-=  |TRANS |           |TRANS | C-Lft| LClk | MsUp | RClk | C-Rt | Vol-Up |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Reset  |SysRq | WlLt | WlDn | WlRt | ???  |------|           |------| Left | MsLt | MsDn | MsRt | Right| Vol-Dn |
+ * | Reset  |SysRq | WlLt | WlDn | WlRt | C--  |------|           |------| Left | MsLt | MsDn | MsRt | Right| Vol-Dn |
  * |--------+------+------+------+------+------| CALC |           | MAIL |------+------+------+------+------+--------|
- * | Scroll | VERSN| Acc0 | Acc1 | Acc2 | ???  |      |           |      | S-Lft| MB4  | MdClk| MB5  | S-Rt | Mute   |
+ * | Scroll | VERSN| Acc0 | Acc1 | Acc2 | C-0  |      |           |      | S-Lft| MB4  | MdClk| MB5  | S-Rt | Mute   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |C-A-6 |C-A-7 |C-A-8 |C-A-9 |C-A-0 |                                       | MUTE | VolUp|VolDn | ???  | TO-L0|
+ *   |C-A-6 |C-A-7 |C-A-8 |C-A-9 |C-A-0 |                                       | ???? | ???  |  ??? | ???  | TO-L0|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |TRANS | TO-1 |       | TO-0 |TRANS |
@@ -117,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MEDIA AND MOUSE
 [MDIA] = KEYMAP(
        LGUI(KC_L), KC_NO, KC_CUT, KC_COPY, KC_PASTE, KC_FIND, KC_TRNS,
-       KC_POWER, KC_NO, KC_UNDO, KC_MS_WH_UP, RCTL(KC_Y), KC_NO, KC_TRNS,
-       RESET, KC_PSCREEN, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_NO,
-       KC_SLCK, M(0), KC_ACL0, KC_ACL1, KC_ACL2, KC_NO, KC_CALCULATOR,
+       KC_POWER, KC_NO, KC_UNDO, KC_MS_WH_UP, RCTL(KC_Y), RCTL(KC_EQUAL), KC_TRNS,
+       RESET, KC_PSCREEN, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, RCTL(KC_MINUS),
+       KC_SLCK, M(0), KC_ACL0, KC_ACL1, KC_ACL2, RCTL(KC_0), KC_CALCULATOR,
        LALT(RCTL(KC_6)),  LALT(RCTL(KC_7)),   LALT(RCTL(KC_8)),   LALT(RCTL(KC_9)),   LALT(RCTL(KC_0)),
                                            KC_TRNS, TO(1),
                                                     KC_TRNS,
@@ -129,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  RCTL(KC_LEFT), KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, RCTL(KC_RIGHT), KC_AUDIO_VOL_UP,
                  KC_LEFT, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT,KC_RIGHT, KC_AUDIO_VOL_DOWN,
        KC_MAIL,  LSFT(KC_LEFT), KC_MS_BTN4, KC_MS_BTN3, KC_MS_BTN5, LSFT(KC_RIGHT), KC_AUDIO_MUTE,
-                          KC__MUTE, KC__VOLUP,  KC__VOLDOWN, KC_NO, TO(0),
+                          KC_NO, KC_NO,  KC_NO, KC_NO, TO(0),
        TO(0), KC_TRNS,
        KC_TRNS,
        KC_NO, KC_TRNS, KC_TRNS
